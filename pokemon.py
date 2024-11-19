3b7755db15ab5dcd683ec42d1475a3968287d1d8
sacha <140752141+heyysacha@users.noreply.github.com>
2024-11-19 19:24:49 +0000

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,8 @@
 import requests
 import random
 
+print("Welcome to the Pokemon challenge!")
+
 def random_pokemon():
     pokemon_number = random.randint(1,151)
     url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
@@ -18,23 +20,50 @@ def random_pokemon():
         'speed': pokemon['stats'][5]['base_stat']
     }
 
-player_pokemon = random_pokemon()
-computer_pokemon = random_pokemon()
+player_score = 0
+computer_score = 0
 
-print("Your Pokemon is number %i: %s. Your opponent's Pokemon is number %i: %s."  % (player_pokemon['id'], player_pokemon['name'], computer_pokemon['id'], computer_pokemon['name']))
+def play_round():
+    player_pokemon = random_pokemon()
+    computer_pokemon = random_pokemon()
 
-while True:
-    player_stat = input("Which stat would you like to use? (Height, weight, HP, attack, defense, or speed): ").lower()
-    if player_stat == 'height' or player_stat == 'weight' or player_stat == 'hp' or player_stat == 'attack' or player_stat == 'defense' or player_stat == 'speed':
-       break
+    global player_score
+    global computer_score
+
+    print("Your Pokemon is number %i: %s."  % (player_pokemon['id'], player_pokemon['name']))
+    print("Height: %i" % (player_pokemon['height']))
+    print("Weight: %i" % (player_pokemon['weight']))
+    print("HP: %i" % (player_pokemon['hp']))
+    print("Attack: %i" % (player_pokemon['attack']))
+    print("Defense: %i" % (player_pokemon['defense']))
+    print("Speed: %i" % (player_pokemon['speed']))
+    print("Your opponent's Pokemon is number %i: %s." % (computer_pokemon['id'], computer_pokemon['name']))
+
+    while True:
+        player_stat = input("Which stat would you like to use? (Height, weight, HP, attack, defense, or speed): ").lower()
+        if player_stat == 'height' or player_stat == 'weight' or player_stat == 'hp' or player_stat == 'attack' or player_stat == 'defense' or player_stat == 'speed':
+           break
+        else:
+            print('Please choose one of the given stats.')
+            continue
+            break
+
+    print("Your %s was: %i, your opponent's %s was: %i." % (player_stat, player_pokemon[player_stat], player_stat, computer_pokemon[player_stat]))
+
+    if player_pokemon[player_stat] > computer_pokemon[player_stat]:
+        print("You win!")
+        player_score = player_score + 1
+    elif computer_pokemon[player_stat] > player_pokemon[player_stat]:
+        print("You lose!")
+        computer_score = computer_score + 1
     else:
-        print('Please choose one of the given stats.')
-        continue
-        break
+        print("Draw!")
+        play_round()
 
-print("Your %s was: %i, your opponent's %s was: %i." % (player_stat, player_pokemon[player_stat], player_stat, computer_pokemon[player_stat]))
+for item in range(3):
+    play_round()
 
-if player_pokemon[player_stat] > computer_pokemon[player_stat]:
-    print("You win!")
+if player_score > computer_score:
+    print("You win! Final score %i to %i" % (player_score, computer_score))
 else:
-    print("You lose!")
+    print("You lose! Better luck next time. Final score %i to %i" % (computer_score, player_score))
